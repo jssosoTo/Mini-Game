a3cf358b44b643d7b7b170674a264b5d2470ae74
P0PC0RN <2982538245@qq.com>
2024-11-05 21:08:33 +0800

    ✨ feat(全局): 新增胜利显示彩条

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import JSConfetti from "js-confetti";
+
+const confetti = new JSConfetti();
+
+export function showConfetti() {
+  console.log("push");
+  confetti.addConfetti();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
